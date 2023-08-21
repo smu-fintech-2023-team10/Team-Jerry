11f62429311253118d0f3aaab93642f6453e3148
Gerald Lee <gerald_4231@hotmail.com>
2023-08-21 21:29:52 +0800

    SS-53
    Completed OCBC APIs for business flows 1. 2 and 3

--- main.py
+++ main.py
@@ -3,48 +3,19 @@ from flask_session import Session  # Import the Session extension
 import requests
 import Constants
 import json
-from twilio.rest import Client
-from twilio.twiml.messaging_response import Message, MessagingResponse
-from flask_ngrok import run_with_ngrok
 import nlp_model
 import helperFunctions
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = 3600  # Set session timeout to 1 hour (in seconds)
-run_with_ngrok(app)
-Session(app)  # Initialize the Session extension
-
-account_sid = Constants.TWILIO_ACCOUNT_SID
-auth_token = Constants.TWILIO_AUTH_TOKEN
-client = Client(account_sid, auth_token)
 
 @app.route("/")
 def get_account_details():
     # session['account_no'] = "201770161001"
     # session['account_type'] = "D"
     session_str = json.dumps(dict(session))
-    helperFunctions.send_message('Hello! Welcome to OCBC Whatsapp Banking. What would you like to do today?')
     return session_str
-
-@app.route("/getReply", methods=['POST'])
-def get_message_reply():
-    incoming_message = request.form['Body']  # Extract the incoming message content
-    print(incoming_message)
-    response = MessagingResponse()
-    message = response.message()  # Create a new message within the response
-    message.body("You said: {incoming_message}. This is your custom response.")  # Set the message content
-    print(str(response))  # Print the TwiML response (including the message)
-    endpoint = nlp_model.generate_reply(incoming_message) #The relevant endpoints will be generated from the model to reply in whatsapp
-    url = Constants.HOST_URL + endpoint
-    res = requests.get(url)
-    print(str(res))
-    return str(response)  # Return the response as the HTTP response
-    
-@app.route("/unableToFindReply")
-def reply_with_none():
-    helperFunctions.send_message('We are unable to find a reply for this.')
-    return
 
 
 # OCBC APIs
@@ -72,7 +43,6 @@ def get_account_balance():
     }
     response = requests.request("GET", url, headers=headers, data=payload)
     print(response.text)
-    helperFunctions.send_message(response.text)
     return response.text
 
 @app.route("/balanceEnquiry")
