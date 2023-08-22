59b909834d38ceb57f63ba7758f31bae8d3af716
Gerald Lee <88034641+geraldljr@users.noreply.github.com>
2023-08-22 20:48:52 +0800

    SS-53 Pull request (#3)
    * SS-53-Sub-Task-2-SP-Integration-and-creating-OCBC-Sandbox-APIs-into-Flask-App
    
    Created OCBC endpoints based on conversation flow.
    
    * Update README.md
    
    * Implemented Message receiving and sending logic
    
    * SS-55 Setup requirements.txt
    
    * SS-53 Setup Requirement.txt (Repush)
    
    * SS-53
    
    Completed OCBC APIs for business flows 1. 2 and 3
    
    ---------
    
    Co-authored-by: carleb <calebcheongdnt@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,131 @@
-from flask import Flask
-
+from flask import Flask, request, session
+from flask_session import Session  # Import the Session extension
+import requests
+import Constants
+import json
+import nlp_model
+import helperFunctions
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600  # Set session timeout to 1 hour (in seconds)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_account_details():
+    # session['account_no'] = "201770161001"
+    # session['account_type'] = "D"
+    session_str = json.dumps(dict(session))
+    return session_str
+
+
+# OCBC APIs
+@app.route("/accountSummary")
+def account_summary():
+    url = "https://api.ocbc.com:8243/transactional/account/1.0/summary*?accountNo="+Constants.ACCOUNT_NO+"&accountType=" +Constants.ACCOUNT_TYPE
+    payload={}
+    headers = {
+    'Authorization': 'Bearer 901b6881-359b-358e-adee-ef6b2bc1a3cd',
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+
+@app.route("/checkBalance")
+def get_account_balance():
+    accountNo = Constants.ACCOUNT_NO
+    url = "https://api.ocbc.com:8243/transactional/accountbalance/1.0/balance*?accountNo=" + accountNo
+    payload={}
+    headers = {
+    'Authorization': 'Bearer ' + Constants.ACCESS_TOKEN
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
+    return response.text
+
+@app.route("/balanceEnquiry")
+def balance_enquiry():
+
+    url = "https://api.ocbc.com:8243/transactional/corp/balance/1.0/enquiry"
+
+    payload = json.dumps({
+    "AccountNo": Constants.ACCOUNT_NO,
+    "AccountType": Constants.ACCOUNT_TYPE,
+    "AccountCurrency": "SGD",
+    "AccountBIC": "OCBCSGSGXXX",
+    "TimeDepositNo": "129"
+    })
+    headers = {
+    'Authorization': 'Bearer ' + Constants.ACCESS_TOKEN,
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+@app.route("/paynow")
+def transfer_money():
+    url = "https://api.ocbc.com:8243/transactional/paynow/1.0/sendPayNowMoney"
+
+    payload = json.dumps({
+    "Amount": 100,
+    "ProxyType": "MSISDN",
+    "ProxyValue": "+6594XXX567",
+    "FromAccountNo": Constants.ACCOUNT_NO
+    })
+    headers = {
+    'Authorization': 'Bearer ' + Constants.ACCESS_TOKEN,
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+@app.route("/paynowEnquiry")
+def paynow_enquiry():
+    url = "https://api.ocbc.com:8243/transactional/paynowenquiry/1.0/payNowEnquiry"
+
+    payload = json.dumps({
+    "ProxyType": "MSISDN", #OR NRIC
+    "ProxyValue": "+6597988922" #OR S9801118H
+    })    
+    headers = {
+    'Authorization': 'Bearer ' + Constants.ACCESS_TOKEN,
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+
+@app.route("/last6MonthsStatement")
+def last_6month_statement():
+    url = "https://api.ocbc.com:8243/transactional/account/1.0/recentAccountActivity*?accountNo="+Constants.ACCOUNT_NO+"&accountType=" +Constants.ACCOUNT_TYPE
+
+    payload={}
+    headers = {
+    'Authorization': 'Bearer ' + Constants.ACCESS_TOKEN
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    return response.text
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
