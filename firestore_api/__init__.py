011d1102e346aa82195aba2de6d542dbea5ed443
wowmiir <syamiir@solana.fm>
2023-08-23 16:41:59 +0800

    SS-52 Migrated Firestore DB URL

--- firestore_api/__init__.py
+++ firestore_api/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 import firebase_admin
 from firebase_admin import db, credentials
+import json
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("firestore_api/key.json")
-firebase_admin.initialize_app(cred, {"databaseURL": "https://team-jerry-default-rtdb.asia-southeast1.firebasedatabase.app/"})
+
+load_dotenv()
+json_key_string = os.getenv("FIRESTORE_API_KEY")
+json_key_dict = json.loads(json_key_string)
+
+cred = credentials.Certificate(json_key_dict)
+firebase_admin.initialize_app(cred, {"databaseURL": "https://smu-fyp-3a677-default-rtdb.asia-southeast1.firebasedatabase.app"})
 
 def create_app():
     app = Flask(__name__)
-
-    from .api import userAPI
-    app.register_blueprint(userAPI, url_prefix='/user')
 
     return app
