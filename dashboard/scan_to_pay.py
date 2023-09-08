81f82fb6b7b45fb37aa21424c9e08242e3c4b5ec
Lam Ching Rou <crlam.2020@smu.edu.sg>
2023-09-09 01:18:16 +0800

    create dashboard
    1. create sidebar in scan_to_pay.py
    2. add csv files
    3. add assets (images)
    4. add overview + business function pages (empty for now except for scan_to_pay)

---
+++ dashboard/scan_to_pay.py
@@ -0,0 +1,58 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+from dash import dcc
+import plotly.express as px
+from dash.dependencies import Input, Output
+import pandas as pd
+import base64
+import os
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    # "background-color": "#081A51",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [   
+        html.Img(src=app.get_asset_url('team_jerry_logo.png'), style={'height':'10%', 'width':'80%'}),
+
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Overview", href="/overview", active="exact"),
+                dbc.NavLink("Check Balance", href="/check-balance", active="exact"),
+                dbc.NavLink("PayNow Transfer", href="/paynow-transfer", active="exact"),
+                dbc.NavLink("Scan To Pay", href="/scan-to-pay", active="exact")
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+app.layout = html.Div([
+    dcc.Location(id="url"),
+    sidebar,
+    content
+])
+
+if __name__=='__main__':
+    app.run_server(debug=True, port=3000)
