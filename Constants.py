5c13b720b8c9cff334a726bbf4213c07a337abe6
wowmiir <syamiir@solana.fm>
2023-10-18 21:19:17 +0800

    merge conflict - added Constants.py

---
+++ Constants.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+HOST_URL = "http://localhost:5000"
+ACCOUNT_NO = "201770161001"
+ACCOUNT_TYPE = "D"
+HEADERS =  {
+    'Authorization': 'Bearer ' + os.environ.get("ACCESS_TOKEN"),
+    'Content-Type': 'application/json'
+    }
+
+OCBC_URL = "https://api.ocbc.com:8243/transactional"
+MESSAGES=[{"role": "system", "content": "You are an OCBC support assistant. Only answer questions related to OCBC. If the question is 'Stop' or 'End' , return 'False' without any other texts or punctuations. Word limit is 1600 characters."}]
+OPENAIENGAGED = False
